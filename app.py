267d030ce35cecadf77d61b902110e233910b70e
Kwabena Adu Adjei <kadjei1@emich.edu>
2026-09-25 14:00:41 -0400

    setup and finalised the create capsule and get capsule api

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask,request
+import uuid
 app = Flask(__name__)
+CAPSULES = []
 
+@app.post("/capsules")
+def create_capsule():
+    data = request.get_json()
+    cid = "cap_" + uuid.uuid4().hex[:6]  #give each capsule an id
+    data["id"] = cid
+    CAPSULES.append(data)
+    return data
+
+   
+@app.get("/capsules")
+def get_capsules():
+    return{"capsules":CAPSULES}
+  
 @app.get("/health")
 def health():
     return {"ok":True}
 app.run(port=5001)
+
